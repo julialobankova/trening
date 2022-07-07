c2fd32d1a67586d623f5b7bdddd8ef60471dded4
Julechka <julialobankova123@gmail.com>
2022-07-07 23:34:14 +0300

    masha

--- trening/Program.cs
+++ trening/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello2, World!");
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine(); 
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine(" Ура, это же маша");
+}
+else 
+{
+     Console.Write("Привет, ");
+     Console.WriteLine(username);
+}
